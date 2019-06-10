e5279e93c8e3534b7c2fe4f8dffede48c1aa4f28
Jonathan Baugh <jonathanatx@gmail.com>
2019-06-10 21:46:13 +0200

    Chnage path

--- bin/assume.sh
+++ bin/assume.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-PYTHON_PATH=$BYHT_DIR_CACHE/assume/venv/bin/python3
-
+PYTHON_PATH=~/.byht/cache/assume/venv/bin/python3
 
 if [[ $@ == *"--help"* ]]; then
   $PYTHON_PATH $BYHT_DIR_CACHE/assume/assume.py $@
