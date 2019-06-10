e979bfb58a0ea6e6578682104fc22cd05bdcb39f
Jonathan Baugh <jonathanatx@gmail.com>
2019-06-10 21:50:05 +0200

    Update paths

--- bin/assume.sh
+++ bin/assume.sh
@@ -3,7 +3,7 @@
 PYTHON_PATH=~/.byht/cache/assume/venv/bin/python3
 
 if [[ $@ == *"--help"* ]]; then
-  $PYTHON_PATH $BYHT_DIR_CACHE/assume/assume.py $@
+  $PYTHON_PATH ~/.byht/cache/assume/assume.py $@
 else
-  $($PYTHON_PATH $BYHT_DIR_CACHE/assume/assume.py $@)
+  $($PYTHON_PATH ~/.byht/cache/assume/assume.py $@)
 fi
